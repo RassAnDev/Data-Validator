c373a07f0cc1e96d7b60df7f3d8d428ae4dcf50b
Andrew <rassandev@gmail.com>
2023-04-29 17:35:12 +0300

    add several new tests to ValidatorTest.java

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -123,6 +123,7 @@ public class ValidatorTest {
         schema.sizeof(2);
 
         assertFalse(schema.isValid(null));
+        assertFalse(schema.isValid(new HashMap<>()));
         assertFalse(schema.isValid(dataMap));
         dataMap.put("key1", "value1");
         dataMap.put("key2", "value2");
@@ -134,7 +135,7 @@ public class ValidatorTest {
     @Test
     public void testMapSchemaWithShapedSchemas() {
         Validator validator = new Validator();
-        MapSchema schema = validator.map();
+        MapSchema schema = validator.map().required();
         Map<String, BaseSchema> schemas = new HashMap<>();
         schemas.put("name", validator.string().required().contains("An").minLength(3));
         schemas.put("age", validator.number().positive().range(10, 50));
@@ -181,6 +182,8 @@ public class ValidatorTest {
         human7.put("age", 22);
         assertFalse(schema.isValid(human7));
 
+        assertFalse(schema.isValid(new HashMap<>()));
+
         Map<String, Object> human8 = new HashMap<>();
         human8.put("name", "Andrew");
         human8.put("age", 25);
@@ -197,20 +200,32 @@ public class ValidatorTest {
         human9.put("age", null);
         assertTrue(schema.isValid(human9));
 
-        Map<String, BaseSchema> schemas2 = new HashMap<>();
-        schemas2.put("name", validator.string().required().minLength(3));
-        schemas2.put("age", validator.number().required().positive());
-
-        schema.shape(schemas2);
+        schema.required();
 
         Map<String, Object> human10 = new HashMap<>();
         human10.put("name", "Andrew");
         human10.put("age", null);
         assertFalse(schema.isValid(human10));
 
+        Map<String, BaseSchema> schemas2 = new HashMap<>();
+        schemas2.put("name", validator.string().required().minLength(3));
+        schemas2.put("age", validator.number().required().positive());
+
+        schema.shape(schemas2);
+
         Map<String, Object> human11 = new HashMap<>();
-        human11.put("name", "An");
-        human11.put("age", 10);
+        human11.put("name", "Andrew");
+        human11.put("age", null);
         assertFalse(schema.isValid(human11));
+
+        Map<String, Object> human12 = new HashMap<>();
+        human12.put("name", "An");
+        human12.put("age", 10);
+        assertFalse(schema.isValid(human12));
+
+        Map<String, Object> human13 = new HashMap<>();
+        human13.put("name", "Andrew");
+        human13.put("age", 25);
+        assertTrue(schema.isValid(human13));
     }
 }
