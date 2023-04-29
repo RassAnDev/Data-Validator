7c50f85bfc0a1073ab045ed551cc737cef7c47fb
Andrew <rassandev@gmail.com>
2023-04-29 17:32:52 +0300

    refactor MapSchema.java

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,50 +1,28 @@
 package hexlet.code.schemas;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class MapSchema extends BaseSchema {
     private static Integer mapSize;
-    private static Map<String, BaseSchema> formedSchemas;
 
     public MapSchema required() {
-        addCheck("required", true);
         hasRequirements = true;
+        addCheck("required", value -> value instanceof Map && !((Map<?, ?>) value).containsValue(null));
         return this;
     }
 
     public MapSchema sizeof(Integer size) {
-        addCheck("sizeof", true);
         mapSize = size;
+        addCheck("sizeof", value -> value instanceof Map && ((Map<?, ?>) value).size() == mapSize);
         return this;
     }
 
     public MapSchema shape(Map<String, BaseSchema> schemas) {
-        addCheck("shape", true);
-        formedSchemas = schemas;
+        this.checks.remove("required");
+
+        addCheck("shape", value -> value instanceof Map
+                && schemas.entrySet().stream()
+                .allMatch(v -> v.getValue().isValid(((Map<?, ?>) value).get(v.getKey()))));
         return this;
-    }
-
-    public boolean validate(Object value) {
-        boolean isMap = value instanceof Map;
-        List<Boolean> passedChecks = new LinkedList<>();
-
-        for (Map.Entry<String, Boolean> check : checks.entrySet()) {
-            if (Objects.equals(check.getKey(), "required")) {
-                passedChecks.add(isMap && !((Map<?, ?>) value).containsValue(null));
-            } else if (Objects.equals(check.getKey(), "sizeof")) {
-                passedChecks.add(isMap && ((Map<?, ?>) value).size() == mapSize);
-            } else if (Objects.equals(check.getKey(), "shape")) {
-                passedChecks.add(isMap && validateShapeSchemas((Map<?, ?>) value));
-            }
-        }
-        return passedChecks.stream().allMatch(v -> v.equals(true));
-    }
-
-    public boolean validateShapeSchemas(Map<?, ?> value) {
-        return formedSchemas.entrySet().stream()
-                .allMatch(v -> v.getValue().isValid(value.get(v.getKey())));
     }
 }
