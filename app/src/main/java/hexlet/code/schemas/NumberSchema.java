d13c45a4f53d6537d717dadbdc0f22b41e036009
Andrew <rassandev@gmail.com>
2023-04-29 17:33:29 +0300

    refactor NumberSchema.java

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,49 +1,24 @@
 package hexlet.code.schemas;
-
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-
 public class NumberSchema extends BaseSchema {
     private static Integer rangeFrom;
     private static Integer rangeTo;
     public NumberSchema required() {
-        addCheck("required", true);
         hasRequirements = true;
+        addCheck("required", value -> value instanceof Number);
         return this;
     }
 
     public NumberSchema positive() {
-        addCheck("positive", true);
+        addCheck("positive", value -> ((value instanceof Number
+                && ((Number) value).intValue() > 0) || value == null));
         return this;
     }
 
     public NumberSchema range(Integer from, Integer to) {
         rangeFrom = from;
         rangeTo = to;
-        addCheck("range", true);
+        addCheck("range", value -> (value instanceof Number && ((Number) value).intValue() >= rangeFrom)
+                && ((Number) value).intValue() <= rangeTo);
         return this;
-    }
-
-    public boolean validate(Object value) {
-        boolean isNumber = value instanceof Number;
-        Integer intValue = 0;
-        List<Boolean> passedChecks = new LinkedList<>();
-
-        if (isNumber) {
-            intValue = ((Number) value).intValue();
-        }
-
-        for (Map.Entry<String, Boolean> check : checks.entrySet()) {
-            if (Objects.equals(check.getKey(), "required")) {
-                passedChecks.add(isNumber);
-            } else if (Objects.equals(check.getKey(), "positive")) {
-                passedChecks.add((isNumber && intValue > 0) || value == null);
-            } else if (Objects.equals(check.getKey(), "range")) {
-                passedChecks.add(isNumber && intValue >= rangeFrom && intValue <= rangeTo);
-            }
-        }
-        return passedChecks.stream().allMatch(v -> v.equals(true));
     }
 }
