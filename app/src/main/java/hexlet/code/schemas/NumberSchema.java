50bc3150585604132e67f509d9ef3c14269843b1
Andrew <rassandev@gmail.com>
2023-05-03 17:53:41 +0300

    refactor NumberSchema.java

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,7 +1,5 @@
 package hexlet.code.schemas;
 public final class NumberSchema extends BaseSchema {
-    private static Integer rangeFrom;
-    private static Integer rangeTo;
     public NumberSchema required() {
         hasRequirements = true;
         addCheck("required", value -> value instanceof Number);
@@ -9,16 +7,13 @@ public final class NumberSchema extends BaseSchema {
     }
 
     public NumberSchema positive() {
-        addCheck("positive", value -> ((value instanceof Number
-                && ((Number) value).intValue() > 0) || value == null));
+        addCheck("positive", value -> ((int) value) > 0);
         return this;
     }
 
-    public NumberSchema range(Integer from, Integer to) {
-        rangeFrom = from;
-        rangeTo = to;
-        addCheck("range", value -> (value instanceof Number && ((Number) value).intValue() >= rangeFrom)
-                && ((Number) value).intValue() <= rangeTo);
+    public NumberSchema range(int min, int max) {
+        addCheck("range", value -> ((int) value) >= min
+                && (((int) value) <= max));
         return this;
     }
 }
