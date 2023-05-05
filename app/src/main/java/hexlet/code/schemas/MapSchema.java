8065158f2be44392fc939ef6e2b90bcfa717b13a
Andrew <rassandev@gmail.com>
2023-05-05 14:15:29 +0300

    refactor MapSchema.java

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public final class MapSchema extends BaseSchema {
     public MapSchema required() {
         hasRequirements = true;
-        addCheck("required", value -> value instanceof Map && !((Map<?, ?>) value).containsValue(null));
+        addCheck("required", value -> value instanceof Map);
         return this;
     }
 
